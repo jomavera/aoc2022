b0ba57f07ca057da948cfde4c2d06768a21f570b
jomavera <josemanuel.vera.aray@gmail.com>
2022-12-06 11:08:11 -0300

    Add day 01 Clojure solution

---
+++ day_01.clj
@@ -0,0 +1,22 @@
+(require '[clojure.java.io :as io])
+
+;; day-01-part-01
+(with-open [rdr (io/reader "input_01.txt")]
+  (let [lines (line-seq rdr) num-rows (- (count lines) 1)]
+    (loop [line (first lines) idx 0 suma-temp 0 lista-calorias []]
+      (if (< idx num-rows)
+        (if (not= line "")
+          (recur (nth lines (inc idx)) (inc idx) (+ suma-temp (Integer/parseInt line)) lista-calorias)
+          (recur (nth lines (inc idx)) (inc idx) 0 (conj lista-calorias suma-temp)))
+        (println (reduce max (conj lista-calorias (+ suma-temp (Integer/parseInt line)))))))))
+
+
+;; day-01-part-02
+(with-open [rdr (io/reader "input_01.txt")]
+  (let [lines (line-seq rdr) num-rows (- (count lines) 1)]
+    (loop [line (first lines) idx 0 suma-temp 0 lista-calorias []]
+      (if (< idx num-rows)
+        (if (not= line "")
+          (recur (nth lines (inc idx)) (inc idx) (+ suma-temp (Integer/parseInt line)) lista-calorias)
+          (recur (nth lines (inc idx)) (inc idx) 0 (conj lista-calorias suma-temp)))
+        (println (reduce + (subvec (vec (reverse (sort (conj lista-calorias (+ suma-temp (Integer/parseInt line)))))) 0 3)))))))
