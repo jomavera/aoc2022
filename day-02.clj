2923de6dcbeae0baa3d6fe353077216dd43e0ad6
jomavera <josemanuel.vera.aray@gmail.com>
2022-12-06 11:49:34 -0300

    Add solution day 02 in Clojure

---
+++ day-02.clj
@@ -0,0 +1,71 @@
+(require '[clojure.java.io :as io])
+
+;; day-02-part-01
+(with-open [rdr (io/reader "resources/input_02.txt")]
+  (let [lines (line-seq rdr) num-rows (- (count lines) 1)]
+    (loop [line (first lines) idx 0 score 0 ]
+      (if (< idx num-rows)
+       (let [[oponent sol] (re-seq #"\w+" line)]
+         (case sol 
+           "X" (case oponent
+                 "A" (recur (nth lines (inc idx)) (inc idx) (+ score 4))
+                 "B" (recur (nth lines (inc idx)) (inc idx) (+ score 1))
+                 "C" (recur (nth lines (inc idx)) (inc idx) (+ score 7)))
+           "Y" (case oponent
+                 "A" (recur (nth lines (inc idx)) (inc idx) (+ score 8))
+                 "B" (recur (nth lines (inc idx)) (inc idx) (+ score 5))
+                 "C" (recur (nth lines (inc idx)) (inc idx) (+ score 2)))
+           "Z" (case oponent
+                 "A" (recur (nth lines (inc idx)) (inc idx) (+ score 3))
+                 "B" (recur (nth lines (inc idx)) (inc idx) (+ score 9))
+                 "C" (recur (nth lines (inc idx)) (inc idx) (+ score 6)))))
+        
+        (let [[oponent sol] (re-seq #"\w+" line)]
+          (println (case sol
+                   "X" (case oponent
+                         "A" (+ score 4)
+                         "B" (+ score 1)
+                         "C" (+ score 7))
+                   "Y" (case oponent
+                         "A" (+ score 8)
+                         "B" (+ score 5)
+                         "C" (+ score 2))
+                   "Z" (case oponent
+                         "A" (+ score 3)
+                         "B" (+ score 9)
+                         "C" (+ score 6)))))))))
+
+;; day-02-part-02
+(with-open [rdr (io/reader "resources/input_02.txt")]
+  (let [lines (line-seq rdr) num-rows (- (count lines) 1)]
+    (loop [line (first lines) idx 0 score 0]
+      (if (< idx num-rows)
+        (let [[oponent sol] (re-seq #"\w+" line)]
+          (case sol
+            "X" (case oponent
+                  "A" (recur (nth lines (inc idx)) (inc idx) (+ score 3))
+                  "B" (recur (nth lines (inc idx)) (inc idx) (+ score 1))
+                  "C" (recur (nth lines (inc idx)) (inc idx) (+ score 2)))
+            "Y" (case oponent
+                  "A" (recur (nth lines (inc idx)) (inc idx) (+ score 4))
+                  "B" (recur (nth lines (inc idx)) (inc idx) (+ score 5))
+                  "C" (recur (nth lines (inc idx)) (inc idx) (+ score 6)))
+            "Z" (case oponent
+                  "A" (recur (nth lines (inc idx)) (inc idx) (+ score 8))
+                  "B" (recur (nth lines (inc idx)) (inc idx) (+ score 9))
+                  "C" (recur (nth lines (inc idx)) (inc idx) (+ score 7)))))
+
+        (let [[oponent sol] (re-seq #"\w+" line)]
+          (println (case sol
+                     "X" (case oponent
+                           "A" (+ score 3)
+                           "B" (+ score 1)
+                           "C" (+ score 2))
+                     "Y" (case oponent
+                           "A" (+ score 4)
+                           "B" (+ score 5)
+                           "C" (+ score 6))
+                     "Z" (case oponent
+                           "A" (+ score 8)
+                           "B" (+ score 9)
+                           "C" (+ score 7)))))))))
