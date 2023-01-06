a816e17594b883c79852f79fd8c87396cfd50e71
jomavera <josemanuel.vera.aray@gmail.com>
2023-01-06 17:26:40 -0300

    Add part 2 of day 10 in julia

--- day_10.jl
+++ day_10.jl
@@ -16,3 +16,18 @@ for line in eachline(io)
 end
 signal = values .* ([x for x in 1:length(values)])
 println(sum(signal[20:40:220]))
+
+# part-02
+line_str = ""
+for (ix, val) in enumerate(values)
+    if ((ix%40)==0)
+        line_str *="\n"
+    else
+        if ((ix-1)%40) in [val-1, val, val+1]
+            line_str *= "#"
+        else
+            line_str *= "."
+        end
+    end
+end
+println(line_str)
